be2c9dd0382aae3f1f85964bb925e2f3acad2329
PatilSushil20 <124390002+PatilSushil20@users.noreply.github.com>
2025-01-09 14:33:12 +0530

    changes for testing.

--- src/components/headerComponents/YouTubeLogo.jsx
+++ src/components/headerComponents/YouTubeLogo.jsx
@@ -6,12 +6,12 @@ const YouTubeLogo = () => {
   return (
     <Link to={"/"}>
       <div className="relative left-2 cursor-pointer">
-        {/* <span
+        <span
           className="absolute -right-2 text-gray-600 top-2"
           style={{ fontSize: "10px" }}
         >
           IN
-        </span> */}
+        </span>
         <div className="w-24 md:w-28">
           <img src={youtubeLogo} alt="YouTube-logo" />
         </div>
